88387611005c8d39d1b403d0e5161fdddea56baf
SAMtoBAM <31311492+SAMtoBAM@users.noreply.github.com>
2025-03-29 10:56:16 -0500

    Add files via upload

---
+++ bin/comparison_paqman_plots.template.R
@@ -0,0 +1,138 @@
+
+################### ################### ################### ################### 
+########################### READ IN LIBRARIES ################################
+################### ################### ################### ################### 
+
+
+packages = c("ggplot2", "reshape2", "ggsci", "ggpubr", "devtools")
+
+##check if a package is installed, if so load, if not install
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+
+##again for ggradar as it need devtools
+packages2=c("ggradar")
+package.check <- lapply(
+  packages2,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      devtools::install_github("ricardo-bion/ggradar", dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+
+
+
+################### ################### ################### ################### 
+###################### COMPARING SUMMARY STATISTICS ###########################
+################### ################### ################### ################### 
+
+### read in summary stats data
+comparisons=read.csv(file="PATHTOSUMMARY", sep='\t', header=T, check.names=FALSE)
+
+##place a combination of the strain and assembly name as the row name
+#rownames(comparisons)=paste(comparisons$strain, comparisons$assembly, sep = "-")
+
+##calculcate BUSCO complete counts as percentages
+comparisons$"BUSCO_complete(%)"=(comparisons$BUSCO_complete/comparisons$BUSCO_total)*100
+comparisons$"BUSCO_complete_single(%)"=(comparisons$BUSCO_complete_single/comparisons$BUSCO_total)*100
+
+
+##set new variable as a combination of the strain and assembly name
+comparisons$label=paste(comparisons$strain, comparisons$assembly, sep = "-")
+
+
+##plots of raw values
+## will generate a set of plots, one being a radar plot of percentages, the rest being the absolute values in side by side columns
+
+##select only the variables that will be compared
+##here we have
+## quast_#contigs
+## quast_assembly_size
+## quast_assembly_N50
+## BUSCO_complete
+## merqury_completeness(%)
+## merqury_qv(phred)
+## merqury_qv(phred)
+## CRAQ_average_CRE(%)
+## CRAQ_average_CSE(%)
+## telomeric_ends
+## t2t_contigs
+comparisonsrad=comparisons[ , c(24, 23, 22, 15, 17, 18, 20), FALSE]
+##tidy up the headers
+names(comparisonsrad) = gsub(pattern = "merqury_", replacement = "", x = names(comparisonsrad))
+names(comparisonsrad) = gsub(pattern = "quast_", replacement = "", x = names(comparisonsrad))
+names(comparisonsrad) = gsub(pattern = "CRAQ_average_", replacement = "", x = names(comparisonsrad))
+
+absplot1=ggradar(comparisonsrad, axis.label.size = 3, legend.text.size = 6, legend.position = "left", group.point.size = 3 , group.line.width = 1, gridline.mid.colour = "grey", grid.label.size = 5, background.circle.colour = "grey90", gridline.mid.linetype = 8 , gridline.max.linetype = 8, grid.max = 100.1, grid.mid=50,  values.radar = c("", "50%", ""))+scale_color_aaas()+coord_cartesian(clip = "off")+theme(plot.margin = margin(0, 5, 0, 5, 'cm'))
+
+comparisonsdot=comparisons[ , c(24, 3, 4, 5, 6, 7, 8, 16, 19, 21), FALSE]
+##remove some of the naming conventions as the labels are too big with them
+names(comparisonsdot) = gsub(pattern = "merqury_", replacement = "", x = names(comparisonsdot))
+names(comparisonsdot) = gsub(pattern = "quast_", replacement = "", x = names(comparisonsdot))
+names(comparisonsdot) = gsub(pattern = "CRAQ_average_", replacement = "", x = names(comparisonsdot))
+
+##melt the dataframe
+comparisonsdot2=melt(comparisonsdot)
+
+##plot 
+absplot2=ggdotchart(comparisonsdot2, x = "label", y="value", group="variable", rotate=T, color="label", facet.by="variable", scales="free_x", nrow=1, add="segment", legend = "none") + theme_pubr(x.text.angle = 45, legend = "none") + xlab("Assembly")+scale_color_aaas()+theme(axis.text.y = element_text(size=10))
+
+
+##combine the two plots together
+absplot3=ggarrange(absplot1, absplot2, ncol=1, heights = c(3,2))
+
+##save the plot
+ggsave(filename = "PATHTOOUTPUT.raw_values.svg", plot = absplot3,
+       width = 500, height = 200, units = "mm", dpi = "retina")
+
+
+
+####RELATIVE VALUES PAQPLOT
+
+##select only the variables that will be compared (all values will be made relative regardless)
+##here we have
+## quast_#contigs
+## quast_assembly_size
+## quast_assembly_N50
+## BUSCO_complete
+## merqury_completeness(%)
+## merqury_qv(phred)
+## merqury_qv(phred)
+## CRAQ_average_CRE(%)
+## CRAQ_average_CSE(%)
+## telomeric_ends
+## t2t_contigs
+comparisonstemp1=comparisons[ , c(5, 6, 3, 15,16,17,18, 19, 21, 22, 23), FALSE]
+##remove some of the naming conventions as the labels are too big with them
+names(comparisonstemp1) = gsub(pattern = "merqury_", replacement = "", x = names(comparisonstemp1))
+names(comparisonstemp1) = gsub(pattern = "quast_", replacement = "", x = names(comparisonstemp1))
+names(comparisonstemp1) = gsub(pattern = "CRAQ_average_", replacement = "", x = names(comparisonstemp1))
+
+
+##get a label to be placed in the first column (taken by ggradar as the label)
+label=as.data.frame(paste(comparisons$strain, comparisons$assembly, sep = "-"))
+##rename column name
+colnames(label)="label"
+
+##calculate the relative values of all samples for each column, dividing by the max for the column
+comparisonstemp2=as.data.frame(apply(comparisonstemp1,2,function(x){x/max(x)}, simplify = F), check.names = F)
+
+##combine the labels (in the first column) with the relative values
+comparisonsrel=cbind(label, comparisonstemp2)
+
+##plot with ggradar
+relplot=ggradar(comparisonsrel, axis.label.size = 3,legend.text.size = 6, legend.position = "top", group.point.size = 3 , group.line.width = 1, gridline.mid.colour = "grey", grid.label.size = 5, background.circle.colour = "grey90", gridline.mid.linetype = 8 , gridline.max.linetype = 8, values.radar = c("", "0.5", ""))+scale_color_aaas()
+
+ggsave(filename = "PATHTOOUTPUT.relative_values.svg", plot = relplot,
+       width = 200, height = 200, units = "mm", dpi = "retina")
---
+++ bin/paqplots.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+set -euo pipefail
+
+version="v1"
+
+##paqman environment
+#mamba create -n paqman bioconda::busco bioconda::merqury bioconda::quast bioconda::filtlong bioconda::seqtk bioconda::craq bioconda::seqkit conda-forge::r-gggenomes conda-forge::r-ggpubr conda-forge::r-ggsci conda-forge::r-svglite conda-forge::r-fmsb conda-forge::r-ggsci conda-forge::r-reshape2
+
+##maybe to add mummer4
+
+
+##to create the conda env using these tools and paqman
+#conda env paqman > paqman.yml
+
+
+paqplotspath=$( which comparison_paqman_plots.template.R )
+
+
+##############################################################
+##################### SETTING VARIABLES ######################
+##############################################################
+
+#default values, unless denoted when running MUM&Co
+summary=""
+list=""
+
+prefix="paqplot"
+output="paqplot_output"
+help="nohelp"
+
+## to clean up a bunch of output from the tools in order to reduce all the unnecessary output
+cleanup="yes"
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case "$key" in
+	-s|--summary)
+	summary="$2"
+	shift
+	shift
+	;;
+	-l|--list)
+	list="$2"
+	shift
+	shift
+	;;
+	-p|--prefix)
+	prefix="$2"
+	shift
+	shift
+	;;
+	-o|--output)
+	output="$2"
+	shift
+	shift
+	;;
+	-c|--cleanup)
+	cleanup="$2"
+	shift
+	shift
+	;;
+	-h|--help)
+	echo "
+	PAQman (version: ${version})
+	paqplot -s summary_file.tsv -p genome -o paqman_output -c yes
+	OR
+	paqplot -l list_of_summary_files.txt -p genome -o paqman_output -c yes
+
+	
+	Required inputs:
+	-s | --summary     A PAQman summary file with multiple assemblies combined and the same header
+	-l | --list		   A list of paths to multiple summary files
+	
+	Optional parameters:
+	-p | --prefix       Prefix for output (default: paqplot)
+	-o | --output       Name of output folder for all results (default: paqplot_output)
+	-c | --cleanup      Remove a large number of files produced by each of the tools that can take up a lot of space. Choose between 'yes' or 'no' (default: 'yes')
+	-h | --help         Print this help message
+	"
+	exit
+	;;
+	esac
+done
+
+#creates error message and exits if these values are not/incorrectly assigned 
+[[ $summary == "" && $list == "" ]] && echo "ERROR: Neither a path to a summary file or list of summary files was given. Provide one with -s or -l respectively." && exit
+
+#some output to notify the use of some parameters
+[[ $summary != "" ]] && echo "PAQman summary file given, using this file"
+##set a full path for the summary file data to be read into the R script
+[[ $summary != "" ]] && summarypath=$( realpath ${summary} )
+
+
+##############################################################
+#################### BEGINNING PAQman-plots ####################
+##############################################################
+echo "###################################################################"
+echo "################## PAQman: Starting PAQman-plots ##################"
+echo "###################################################################"
+echo "################## PAQman: Step 1a: Collecting stats from summary files"
+
+
+## assembly evaluations
+mkdir ${output}
+
+
+##create combined summary file if provided (-l) and -s was empty
+
+if [[ ${summary} == "" && ${list} != "" ]]
+then
+echo "prefix;assembly;quast_#contigs;quast_#contigs>10kb;quast_assembly_size;quast_assembly_N50;quast_assembly_N90;quast_largest_contig;BUSCO_db;BUSCO_total;BUSCO_complete;BUSCO_complete_single;BUSCO_fragmented;BUSCO_missing;merqury_completeness(%);merqury_qv(phred);CRAQ_average_CRE(%);CRAQ_average_CSE(%);telomeric_ends;telomeric_ends(%);t2t_contigs" | tr ';' '\t' >  ${output}/combined.summary_stats.tsv
+cat ${list} | while read file
+do
+tail -n+2 ${file} >> ${output}/combined.summary_stats.tsv
+done
+summarypath=$( realpath ${output}/combined.summary_stats.tsv )
+fi
+
+cd ${output}
+
+
+
+echo "################## PAQman: Step 1b: Plotting comparisons"
+##covert paths in R script to those relevant for the coverage outputs generated here and then export the plots 
+cat ${paqplotspath} | sed "s|PATHTOSUMMARY|${summarypath}|" | sed "s|PATHTOOUTPUT|./${prefix}|" > ./${prefix}.paqman_plots.R
+Rscript ./${prefix}.paqman_plots.R
+
+
+echo "################## PAQman: PAQman plots can be found here ${output}/*.svg"
+echo "################## PAQman: All complete; thanks for using PAQman"
+
