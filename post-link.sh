f0e1d0267fc5728bc28dec35be8509f0373259aa
SAMtoBAM <31311492+SAMtoBAM@users.noreply.github.com>
2025-07-11 15:11:49 -0500

    Update post-link.sh

--- post-link.sh
+++ post-link.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+##modify craq paths
 sed -i 's|/../src/|/../share/CRAQ/src/|g' "$PREFIX/bin/craq"
+
+##install ggradar
+Rscript -e "devtools::install_github("ricardo-bion/ggradar", dependencies = TRUE)"
