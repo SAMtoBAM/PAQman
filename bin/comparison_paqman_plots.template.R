a0b7c9211ca84a617b96c6ea91061af7e1d0210f
SAMtoBAM <31311492+SAMtoBAM@users.noreply.github.com>
2025-07-17 10:04:18 -0500

    Update comparison_paqman_plots.template.R
    reduce size of raw_values plot for larger relative labels etc

--- bin/comparison_paqman_plots.template.R
+++ bin/comparison_paqman_plots.template.R
@@ -113,7 +113,7 @@ absplot3=ggarrange(absplot1, absplot2, ncol=1, heights = c(3,2))
 
 ##save the plot
 ggsave(filename = "PATHTOOUTPUT.raw_values.svg", plot = absplot3,
-       width = 500, height = 250, units = "mm", dpi = "retina")
+       width = 350, height = 175, units = "mm", dpi = "retina")
 
 
 
