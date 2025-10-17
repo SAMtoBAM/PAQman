4eab59913dfae46027ec1a7a36fe90ccb0273a7f
SAMtoBAM <31311492+SAMtoBAM@users.noreply.github.com>
2025-10-17 13:25:59 -0500

    Update coverage_plots.template_LR.R
    check that the contigs in the coverage input have at least >1 data points for geom_line()

--- bin/coverage_plots.template_LR.R
+++ bin/coverage_plots.template_LR.R
@@ -21,10 +21,14 @@ library(svglite)
 
 #### read in the normalised coverage plots
 LRcoverage=read.csv(file="PATHTOLRCOVERAGE", header=T, sep='\t' )
+
+##make sure that each contig has at least >1 data point
+LRcoverage_filtered <- subset(LRcoverage, ave(seq_along(contig), contig, FUN = length) > 1)
+
 #### generate the plot of the relative coverage
 ## added lines donoting a 1X relative coverage and 0.5; no higher as I don't know if it'll be within the scale)
 ## can easily add higher by copying the geom_hline and changing the yintercept value
-LRplot=ggplot(data=LRcoverage, aes(x=((start+end)/2), y=coverage_norm ))+
+LRplot=ggplot(data=LRcoverage_filtered, aes(x=((start+end)/2), y=coverage_norm ))+
   geom_hline(yintercept = 1, linetype="dashed", colour="red")+
   geom_line()+
   facet_wrap(.~ contig , ncol=2, scales="free_y", )+
@@ -35,7 +39,7 @@ LRplot=ggplot(data=LRcoverage, aes(x=((start+end)/2), y=coverage_norm ))+
 
 #### output the plot as an svg
 ##can have issues with the plot size depending on the number of contig so calculate a plot height relative to that number
-mmhigh=(length(unique(LRcoverage$contig))/2*50)
+mmhigh=(length(unique(LRcoverage_filtered$contig))/2*50)
 ##output with width of just a bit less than A4 and DPI of 320
 ggsave(filename = "PATHTOOUTPUT.LR.svg", plot = LRplot,
        width = 200, height = mmhigh, units = "mm", dpi = "retina", limitsize = F)
