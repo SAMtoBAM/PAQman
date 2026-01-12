44ef65d7d82e44821f92c23c3fd6be4c734e7cc8
SAMtoBAM <31311492+SAMtoBAM@users.noreply.github.com>
2026-01-12 14:47:49 -0600

    Update post-link.sh

--- post-link.sh
+++ post-link.sh
@@ -3,6 +3,10 @@
 ##modify craq paths
 sed -i 's|/../src/|/../share/CRAQ/src/|g' "$PREFIX/bin/craq"
 
+##add multithreading to CRAQ samtools depth step
+sed -i "s/samtools depth -a/samtools depth -@ \$t -a/" "$PREFIX/src/runLR.sh"
+sed -i "s/samtools depth -a/samtools depth -@ \$t -a/" "$PREFIX/src/runSR.sh"
+
 ##install ggradar (now doing by conda)
 #Rscript -e "devtools::install_github("ricardo-bion/ggradar", dependencies = TRUE)"
 
